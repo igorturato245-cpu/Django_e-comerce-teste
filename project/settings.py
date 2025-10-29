f5418d2cebfa49611f64eb7cbd77adbaa4baaffd
Igor Martins <igorturato@gmail.com>
2025-10-29 14:04:59 -0300

    Configurando git/Criando o app/Criando arquivos estaticos e configurnando eles em settings.py/Superuser

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'e_comerce',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,6 +114,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static',
+]
 
 STATIC_URL = 'static/'
 
