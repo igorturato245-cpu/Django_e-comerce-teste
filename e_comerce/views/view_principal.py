630259e85d655104277ed8152e8c89d5fec50f95
Igor Martins <igorturato@245gmail.com>
2025-12-08 16:57:03 -0300

    Criando e ajeitando a seções do index

--- e_comerce/views/view_principal.py
+++ e_comerce/views/view_principal.py
@@ -2,11 +2,17 @@ from django.shortcuts import render,get_list_or_404,redirect
 from e_comerce.models import Produto
 
 def index(request):
-    produtos=Produto.objects.order_by('-id')
+    perfumes=Produto.objects.filter(category__slug='perfumes-',disponivel=True)
+    sabonetes=Produto.objects.filter(category__slug='sabonete-',disponivel=True),
+    produto_de_limpeza=Produto.objects.filter(category__slug='limpeza-',disponivel=True),
+
     ofertas_do_dia=Produto.objects.filter(ofertas_do_dia=True,disponivel=True)[:8]
 
     context={
-        'produtos':produtos,
+        'perfumes':perfumes,
+        'sabonetes':sabonetes,
+        'porduto_de_limpeza':produto_de_limpeza,
+
         'ofertas_do_dia':ofertas_do_dia,
     }
 
