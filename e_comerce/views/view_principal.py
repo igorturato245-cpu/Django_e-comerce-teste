f2de3c7d535be2689afc56b8cd6e34d1897fa7e9
Igor Martins <igorturato@245gmail.com>
2025-12-10 19:46:19 -0300

    Terminando a parte sem login do index

--- e_comerce/views/view_principal.py
+++ e_comerce/views/view_principal.py
@@ -2,16 +2,17 @@ from django.shortcuts import render,get_list_or_404,redirect
 from e_comerce.models import Produto
 
 def index(request):
-    perfumes=Produto.objects.filter(category__slug='perfumes-',disponivel=True)
-    sabonetes=Produto.objects.filter(category__slug='sabonete-',disponivel=True),
-    produto_de_limpeza=Produto.objects.filter(category__slug='limpeza-',disponivel=True),
+    perfumes=Produto.objects.filter(category__slug='perfume-',disponivel=True)
 
-    ofertas_do_dia=Produto.objects.filter(ofertas_do_dia=True,disponivel=True)[:8]
+    sabonetes=Produto.objects.filter(category__slug='sabonete-',disponivel=True)
+    produto_de_limpeza=Produto.objects.filter(category__slug='limpeza-',disponivel=True)
+
+    ofertas_do_dia=Produto.objects.filter(ofertas_do_dia=True,disponivel=True)[:5]
 
     context={
         'perfumes':perfumes,
         'sabonetes':sabonetes,
-        'porduto_de_limpeza':produto_de_limpeza,
+        'produto_de_limpeza':produto_de_limpeza,
 
         'ofertas_do_dia':ofertas_do_dia,
     }
