90c2c2bea049ae9bc70b4cc594239cbb118ed227
Igor Martins <igorturato@245gmail.com>
2025-12-11 17:18:23 -0300

    Começando o template do single product

--- e_comerce/urls.py
+++ e_comerce/urls.py
@@ -3,10 +3,10 @@ from e_comerce.views import view_principal
 from e_comerce.views import view_produto
 from e_comerce.views import view_carrinho
 
-app_name='Produtos'
+app_name='produtos'
 
 urlpatterns = [
     path('', view_principal.index, name='index'),
-    path('produto/<int:produto_id>', view_produto.produto, name='Produto'),
+    path('produto/<int:produto_id>', view_produto.produto, name='produto'),
     path('carrinho/<int:carrinho_id>',view_carrinho.carrinho,name='carrinho')
 ]
--- e_comerce/views/view_produto.py
+++ e_comerce/views/view_produto.py
@@ -3,9 +3,11 @@ from e_comerce.models import Produto
 
 def produto(request,produto_id):
     sigle_product=get_object_or_404(Produto,pk=produto_id,disponivel=True)
+    ofertas_do_dia=Produto.objects.filter(ofertas_do_dia=True,disponivel=True)
 
     context={
         'produto':sigle_product,
+        'ofertas_do_dia':ofertas_do_dia,
     }
 
     return render(
