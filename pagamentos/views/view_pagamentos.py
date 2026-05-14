80e73942e915fe195c9e1f633fe3ca1beaf36d0d
Igor Martins <igorturato245@gmail.com>
2026-05-14 13:13:03 -0300

    Tentando novamente

--- pagamentos/views/view_pagamentos.py
+++ pagamentos/views/view_pagamentos.py
@@ -283,7 +283,7 @@ def pagseguro_notification(request):
 @require_api_erp 
 @require_api_payment  
 def checkout(request):
-    return redirect('institucional:manutencao') 
+    #return redirect('institucional:manutencao') 
     
     if not request.user.is_authenticated:
         return redirect('cadastro_login:loginuser')
