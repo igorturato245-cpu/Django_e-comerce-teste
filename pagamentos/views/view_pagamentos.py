51f7ee8192898501b810adb636622ff49d1b28ad
Igor Martins <igorturato245@gmail.com>
2026-05-15 12:28:38 -0300

    Trava de segurança

--- pagamentos/views/view_pagamentos.py
+++ pagamentos/views/view_pagamentos.py
@@ -283,6 +283,7 @@ def pagseguro_notification(request):
 @require_api_erp 
 @require_api_payment  
 def checkout(request): 
+    return redirect('produtos:manutencao')
     
     if not request.user.is_authenticated:
         return redirect('cadastro_login:loginuser')
